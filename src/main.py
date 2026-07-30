bf5ae0013c86496822de3b5a660dd860fff0d575
matthewclay88 <matthew.clay@noaa.gov>
2026-07-29 22:16:12 -0400

    Add main.py as application entry point

---
+++ src/main.py
@@ -0,0 +1,27 @@
+"""
+SA Display Manager
+main.py
+
+Application entry point.
+"""
+
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from ui import MainWindow
+
+
+def main():
+    """Start the application."""
+
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
